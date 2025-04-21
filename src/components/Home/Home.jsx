04cc60ac24941a6d5a1c314ec6e8914dc130c04c
Essoh <nessoh1007@gmail.com>
2025-04-21 01:21:18 +0100

    Changed Profile Image to addFile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Home.css'
-import Profile from "../../assets/profile.png"
+import profileImg from '../../assets/addFile.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Home = () => {
   return (
     <div id='home' className='home'>
-      <img src={Profile} alt="Essoh" />
+      <img src={profileImg} alt="Essoh" />
       
         <h1><span>I'm Nsikan Essoh,</span><br /> Software Developer based in Nigeria.</h1>
         <p>3+ years of experience building scalable web applications and leading development teams. Seeking to bring my expertise in Mobile App Development, responsive web application, cloud technologies, and eCommerce platforms like Shopify to organizations looking to drive innovative SaaS solutions and optimize development processes.
