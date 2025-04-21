7e3e1a887cc1ffb1636364c291905ff8bb60a4c9
Essoh <nessoh1007@gmail.com>
2025-04-21 01:06:36 +0100

    npm run build

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,16 +8,6 @@ const Footer = () => {
       <hr />
       <div className="footer_bottom">
         <p className='footer_bottom_left'>© 2024 Nsikan Essoh. All rights reserved.</p>
-        {/* <div className="footer_bottom_right">
-          <ul>
-            <li>
-              <Link to="/terms-of-service" className="footer_link">Terms of Service</Link>
-            </li>
-            <li>
-              <Link to="/privacy-policy" className="footer_link">Privacy Policy</Link>
-            </li>
-          </ul>
-        </div> */}
       </div>
     </div>
   );
